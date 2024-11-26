3dc7f61cb45f49876cf7dcde84e613743404d45a
Алексей Панцерновский <pan_alexey@mail.ru>
2024-11-27 02:09:19 +0600

    Добавлена зависимость от AuthenticationService в контроллер AuthenticationController

--- project/libs/account/authentication/src/authentication-module/authentication.controller.ts
+++ project/libs/account/authentication/src/authentication-module/authentication.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common';
 
+import { AuthenticationService } from './authentication.service';
+
 @Controller('auth')
-export class AuthenticationController {}
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+}
