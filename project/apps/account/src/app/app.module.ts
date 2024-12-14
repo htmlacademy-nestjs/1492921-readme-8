aed1a3ad04835b5a87e35c3578a1e77774db17ed
Алексей Панцерновский <pan_alexey@mail.ru>
2024-12-14 21:41:48 +0600

    Извлекаем данные с помощью ConfigService

--- project/apps/account/src/app/app.module.ts
+++ project/apps/account/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BlogUserModule } from '@project/blog-user';
 import { AuthenticationModule } from '@project/authentication';
+import { AccountConfigModule } from '@project/account-config';
 
 @Module({
-  imports: [BlogUserModule, AuthenticationModule],
+  imports: [BlogUserModule, AuthenticationModule, AccountConfigModule],
   controllers: [],
   providers: [],
 })
