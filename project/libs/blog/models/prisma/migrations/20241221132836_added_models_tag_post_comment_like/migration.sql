da5db32a4a4b1bbccb0294b54d1a4a2ff50f3a50
Алексей Панцерновский <pan_alexey@mail.ru>
2024-12-21 19:33:50 +0600

    Добавит модели Post, Comment, Like, Tag

---
+++ project/libs/blog/models/prisma/migrations/20241221132836_added_models_tag_post_comment_like/migration.sql
@@ -0,0 +1,93 @@
+-- CreateEnum
+CREATE TYPE "PostType" AS ENUM ('Video', 'Text', 'Quote', 'Photo', 'Link');
+
+-- CreateTable
+CREATE TABLE "posts" (
+    "id" TEXT NOT NULL,
+    "post_type" "PostType" NOT NULL,
+    "author_id" TEXT NOT NULL,
+    "repost_id" TEXT,
+    "create_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "public_date" TIMESTAMP(3) NOT NULL,
+    "likes_count" INTEGER NOT NULL DEFAULT 0,
+    "comments_count" INTEGER NOT NULL DEFAULT 0,
+    "name" TEXT,
+    "url" TEXT,
+    "preview" TEXT,
+    "text" TEXT,
+    "quote_author_id" TEXT,
+    "quote_text" TEXT,
+    "description" TEXT,
+
+    CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tags" (
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "tags_pkey" PRIMARY KEY ("name")
+);
+
+-- CreateTable
+CREATE TABLE "comments" (
+    "id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "create_date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "comments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "likes" (
+    "user_id" TEXT NOT NULL,
+    "post_id" TEXT NOT NULL,
+
+    CONSTRAINT "likes_pkey" PRIMARY KEY ("user_id","post_id")
+);
+
+-- CreateTable
+CREATE TABLE "_PostToTag" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL,
+
+    CONSTRAINT "_PostToTag_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "posts_author_id_idx" ON "posts"("author_id");
+
+-- CreateIndex
+CREATE INDEX "posts_create_date_idx" ON "posts"("create_date");
+
+-- CreateIndex
+CREATE INDEX "posts_likes_count_idx" ON "posts"("likes_count");
+
+-- CreateIndex
+CREATE INDEX "posts_comments_count_idx" ON "posts"("comments_count");
+
+-- CreateIndex
+CREATE INDEX "tags_name_idx" ON "tags"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "likes_user_id_post_id_key" ON "likes"("user_id", "post_id");
+
+-- CreateIndex
+CREATE INDEX "_PostToTag_B_index" ON "_PostToTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_repost_id_fkey" FOREIGN KEY ("repost_id") REFERENCES "posts"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "comments" ADD CONSTRAINT "comments_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "likes" ADD CONSTRAINT "likes_post_id_fkey" FOREIGN KEY ("post_id") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "posts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "tags"("name") ON DELETE CASCADE ON UPDATE CASCADE;
