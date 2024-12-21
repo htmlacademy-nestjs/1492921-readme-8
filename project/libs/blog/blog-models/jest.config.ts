45273f730241c72c7d24e93036b1c5234bdc4f70
Алексей Панцерновский <pan_alexey@mail.ru>
2024-12-21 23:42:59 +0600

    Добавит базовый репозиторий для работы с PostgreSQL. Перенос библиотек в другие папки

--- project/libs/blog/blog-models/jest.config.ts
+++ project/libs/blog/blog-models/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/blog/models',
+  coverageDirectory: '../../../coverage/libs/blog/blog-models',
 };
