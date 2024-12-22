b4aa2050b7e2421329004cf96092c3db317fa1b6
Алексей Панцерновский <pan_alexey@mail.ru>
2024-12-22 22:09:59 +0600

    Добавит базовую валидацию для сервиса account

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,29 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsString, IsUrl } from 'class-validator';
+import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
 
 export class CreateUserDto {
   @ApiProperty({
     description: 'User unique email',
     example: 'user@user.ru',
   })
+  @IsEmail({}, { message: AuthenticationValidateMessage.EmailNotValid })
   public email: string;
   @ApiProperty({
     description: 'User login',
     example: 'user',
   })
+  @IsString()
   public login?: string;
   @ApiProperty({
     description: 'User name',
     example: 'Ivan Ivanov',
   })
+  @IsString()
   public name?: string;
   @ApiProperty({
     description: 'User avatar',
     example: 'https://16.design.htmlacademy.pro/static/avatar/5.jpg',
   })
+  @IsUrl()
   public avatarUrl?: string;
   @ApiProperty({
     description: 'User password',
     example: '123456',
   })
+  @IsString()
   public password: string;
 }
