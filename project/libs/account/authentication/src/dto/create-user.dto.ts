07211ba27a92e81a750c5a60c5a9e05699901023
Алексей Панцерновский <pan_alexey@mail.ru>
2025-01-06 17:53:29 +0600

    Добавит поддержку JWT в AuthenticationController

--- project/libs/account/authentication/src/dto/create-user.dto.ts
+++ project/libs/account/authentication/src/dto/create-user.dto.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsString, IsUrl } from 'class-validator';
+import { IsEmail, IsString, IsUrl, ValidateIf } from 'class-validator';
 import { AuthenticationValidateMessage } from '../authentication-module/authentication.constant';
+import { Optional } from '@nestjs/common';
 
 export class CreateUserDto {
   @ApiProperty({
@@ -25,6 +26,7 @@ export class CreateUserDto {
     description: 'User avatar',
     example: 'https://16.design.htmlacademy.pro/static/avatar/5.jpg',
   })
+  @ValidateIf((o) => o.avatarUrl)
   @IsUrl()
   public avatarUrl?: string;
   @ApiProperty({
