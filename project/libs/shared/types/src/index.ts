9a5aa627e38edd914882cea7a006e908e2fc4784
Алексей Панцерновский <pan_alexey@mail.ru>
2025-01-06 17:18:33 +0600

    Добавит интерфейсы Token и TokenPayload

--- project/libs/shared/types/src/index.ts
+++ project/libs/shared/types/src/index.ts
@@ -3,6 +3,8 @@ export { StorableEntity } from './lib/interfaces/storable-entity.interface';
 export { EntityFactory } from './lib/interfaces/entity-factory.interface';
 export { PaginationResult } from './lib/interfaces/pagination.interface';
 export { SortDirection } from './lib/interfaces/sort-direction.interface';
+export { Token } from './lib/interfaces/token.interface';
+export { TokenPayload } from './lib/interfaces/token-payload.interface';
 
 export { User } from './lib/types/user.interface';
 export { AuthUser } from './lib/types/auth-user.interface';
---
+++ project/libs/shared/types/src/lib/interfaces/token-payload.interface.ts
@@ -0,0 +1,6 @@
+export interface TokenPayload {
+  sub: string;
+  email: string;
+  login: string;
+  name: string;
+}
