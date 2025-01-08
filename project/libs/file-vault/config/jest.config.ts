748d006c85b357e01a03900b2e635580d50e1554
Алексей Панцерновский <pan_alexey@mail.ru>
2025-01-08 11:58:34 +0600

    Добавит конфигурацию для file-vault

--- project/libs/file-vault/config/jest.config.ts
+++ project/libs/file-vault/config/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: 'account-config',
+  displayName: 'file-vault-config',
   preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/account/config',
+  coverageDirectory: '../../../coverage/libs/file-vault/config',
 };
